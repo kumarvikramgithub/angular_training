fe8c97c209f944983f6ae91fe8c730f3c3ffd598
KumarV <kumar.vikram@citiustech.com>
2022-01-12 12:24:13 +0530

    till input output added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
+import { UserImageComponent } from './components/users/user-image/user-image.component';
+import { UserInfoComponent } from './components/users/user-info/user-info.component';
 
 @NgModule({
-  declarations: [
+  declarations: [     // Component / Directive / Pipe
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserImageComponent,
+    UserInfoComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [          // Modules - Built-in or Custom
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [],      // Service Registration
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,18 +1,25 @@
-import { Component, ViewEncapsulation } from "@angular/core";
+import { Component, ViewEncapsulation } from '@angular/core';
+
+
 @Component({
-    selector : "app-users",
-    templateUrl : `./users.component.html`,
-    styleUrls :[`./users.component.css`],
-    encapsulation :ViewEncapsulation.ShadowDom,
+  selector : "app-users",
+  templateUrl : `./users.component.html`,
+  styleUrls : [`./users.component.css`],
+  encapsulation : ViewEncapsulation.Emulated
 })
 export class UsersComponent{
-    user = {
-        firstName: "bill",
-        lastName:"gates",
-        bio:"Bill was a founder of microsoft",
-        isWorking : true,
-        income : 50000,
-        imagePath : "./assets/images/bill.jpeg",
-        votes : 130
-    }
- }
+  user = {
+    firstName : "bill",
+    lastName : "gates",
+    dob : new Date("Dec 21, 1965"),
+    isWorking : true,
+    income : 50000,
+    company : "Microsoft",
+    imagePath : "./assets/images/bill.jpeg",
+    votes : 130
+  }
+
+  onMoreInfo(lastName : string, company : string){
+    alert(`Mr. ${lastName} is working with ${company}`)
+  }
+}
